183c1fde5783861c08c886672159a4494bf5e2e5
Mieszko Cichon <mieszkocichon@gmail.com>
2018-05-24 18:57:23 +0200

    Levenshtein distance tests.

--- test/levenshtein_distance_test.dart
+++ test/levenshtein_distance_test.dart
@@ -30,7 +30,6 @@ void main() {
     int result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 0);
 
     _finalInscription = "Sunday";
@@ -38,70 +37,60 @@ void main() {
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 3);
 
     _primitiveInscription = ["S", "a", "t", "u", "r", "d", "a", "y"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 3);
 
     _primitiveInscription = ["S", "a", "t", "u", "r", "d", "a"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 4);
 
     _primitiveInscription = ["S", "a", "t", "u", "r", "d"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 5);
 
     _primitiveInscription = ["S", "a", "t", "u", "r"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 5);
 
     _primitiveInscription = ["S", "a", "t", "u"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 5);
 
     _primitiveInscription = ["S", "a", "t"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 4);
 
     _primitiveInscription = ["S", "a"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 4);
 
     _primitiveInscription = ["S"];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 5);
 
     _primitiveInscription = [];
     result = (
         new LevenshteinDistance(_finalInscription, _primitiveInscription)
     ).build();
-
     expect(result, 6);
   });
 }
